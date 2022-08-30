b580f1be5df30d92361dcbfacadf619549c7d11c
AtaMeydani <miratameydani@gmail.com>
2022-08-31 00:30:12 +0430

    Add Chapter_06

---
+++ chapter_06/android/app/src/main/kotlin/com/miratameydani/chapter_06/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.miratameydani.chapter_06
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chapter_06/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'plan_provider.dart';
+import 'views/plan_creator_screen.dart';
+
+void main() => runApp(PlanProvider(child: const MasterPlanApp()));
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple, cardColor: Colors.purpleAccent),
+      home: const PlanCreatorScreen(),
+    );
+  }
+}
